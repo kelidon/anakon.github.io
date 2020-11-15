6ab408a70e77a0f517be8dbb2d3cb712063a3177
kelidon <v.shchors@gmail.com>
2020-11-16 01:38:23 +0300

    upd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,9 +43,9 @@ const RESOURCES = {
 "icons/favicon2.png": "340aacff202dab195093452009e04e09",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "12a02400ee034e687fd553bd483ce6ec",
-"/": "12a02400ee034e687fd553bd483ce6ec",
-"main.dart.js": "05ef637916ac62b009dade78dc4aa540",
+"index.html": "1f0c778affb355156d9103576569aa18",
+"/": "1f0c778affb355156d9103576569aa18",
+"main.dart.js": "3a09895b13c3640b20e360db481cd534",
 "manifest.json": "edec10d4a54b61be5dd08497f4fc29dd",
 "version.json": "01c1abd99ff96808711d37ec4b2d72f0"
 };
